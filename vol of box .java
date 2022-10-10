ddaf99c71054302a4724b8f3ba045b6400cb4d7e
Utsav Raj <105374553+Utsav9053@users.noreply.github.com>
2022-10-10 10:57:18 +0530

    Create vol of box .java

---
+++ vol of box .java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+ public class Main
+{
+      
+   public static void main(String args[]) 
+    {   
+       	Scanner s= new Scanner(System.in);
+       	System.out.println("Enter the length of box:");
+    	double l=s.nextDouble();
+		System.out.println("Enter the breadth of box:");
+		double b=s.nextDouble();
+		System.out.println("Enter height of box:");
+        double h=s.nextDouble();
+        double  volume= l*b*h;
+        System.out.println("Volume Of box is:" +volume);
+   }
+}
